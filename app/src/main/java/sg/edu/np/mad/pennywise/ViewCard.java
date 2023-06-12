43996484b65e662d70017d24a1f3e787c7b0fb01
Kenan Chia <Kenan Chia@LAPTOP-04G3VC6V>
2023-06-12 20:51:57 +0800

    Add View Card XML plus link to navbar

---
+++ app/src/main/java/sg/edu/np/mad/pennywise/ViewCard.java
@@ -0,0 +1,26 @@
+package sg.edu.np.mad.pennywise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class ViewCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_card);
+
+        ImageView homeBtn = findViewById(R.id.ViewCardBtn);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ViewCard.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
