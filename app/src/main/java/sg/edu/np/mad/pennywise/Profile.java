8e26fbde85da3b48394eaf2740089be87bdfa142
Wen Bin Koh <s10242132@connect.np.edu.sg>
2023-06-13 13:03:17 +0800

    Create Edit Profile XML did some validation for empty values

--- app/src/main/java/sg/edu/np/mad/pennywise/Profile.java
+++ app/src/main/java/sg/edu/np/mad/pennywise/Profile.java
@@ -1,9 +1,12 @@
 package sg.edu.np.mad.pennywise;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -19,6 +22,16 @@ public class Profile extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Profile.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView EditButton = findViewById(R.id.Profile_Edit);
+        EditButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "Editting Profile!!");
+                Intent intent = new Intent(Profile.this, EditProfile.class);
                 startActivity(intent);
             }
         });
