51d65b1127ed957b6d6cbce64df884a317b08593
Xin Yin <xinyin322@gmail.com>
2023-06-14 21:00:32 +0800

    added items for dashboard activity

--- app/src/main/java/sg/edu/np/mad/pennywise/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/pennywise/MainActivity.java
@@ -8,19 +8,38 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.ClipData;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.TextView;
 //import android.widget.Toolbar;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     DrawerLayout drawerLayout;
     NavigationView navigationView;
     Toolbar toolbar;
+
+    //Shared preference
+    public String GLOBAL_PREFS = "myPrefs";
+    public String MY_EMAIL = "MyEmail";
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +60,62 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         navigationView.setNavigationItemSelectedListener(this);
         navigationView.setCheckedItem(R.id.nav_home);
+        getBalance();
+        getDashboardItems();
+    }
+
+    public void getDashboardItems(){
+        sharedPreferences = getSharedPreferences(GLOBAL_PREFS, MODE_PRIVATE);
+        String sharedEmail = sharedPreferences.getString(MY_EMAIL, "");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference transactionRef = db.collection("users").document(sharedEmail).collection("alltransaction");
+        transactionRef.get().addOnCompleteListener(task -> {
+            QuerySnapshot querySnapshot = task.getResult();
+            List<DocumentSnapshot> documents = querySnapshot.getDocuments();
+            ArrayList<Transaction> transactionList = new ArrayList<>();
+            for (DocumentSnapshot document : documents){
+                Map<String, Object> data = document.getData();
+
+                //Extract data
+                String title = (String) data.get("title");
+                String date = (String) data.get("date");
+                double amount = (double) data.get("amount");
+                String type = (String) data.get("type");
+                Transaction transaction = new Transaction(title, date, amount, type);
+                transactionList.add(transaction);
+            }
+            RecyclerView recyclerView = findViewById(R.id.dashboardRecyclerView);
+            DashboardAdaptor dashboardAdaptor = new DashboardAdaptor(transactionList);
+            LinearLayoutManager myLayoutManager = new LinearLayoutManager(this);
+            recyclerView.setLayoutManager(myLayoutManager);
+            recyclerView.setItemAnimator(new DefaultItemAnimator());
+            recyclerView.setAdapter(dashboardAdaptor);
+        });
+    }
+
+    public void getBalance(){
+        sharedPreferences = getSharedPreferences(GLOBAL_PREFS, MODE_PRIVATE);
+        String sharedEmail = sharedPreferences.getString(MY_EMAIL, "");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        CollectionReference transactionRef = db.collection("users").document(sharedEmail).collection("alltransaction");
+        transactionRef.get().addOnCompleteListener(task -> {
+            QuerySnapshot querySnapshot = task.getResult();
+            List<DocumentSnapshot> documents = querySnapshot.getDocuments();
+            double totalBalance = 0;
+            for (DocumentSnapshot document : documents){
+                Map<String, Object> data = document.getData();
+                double amount = (double) data.get("amount");
+                String type = (String) data.get("type");
+                if (type.equals("income")){
+                    totalBalance += amount;
+                }
+                else{
+                    totalBalance -= amount;
+                }
+            }
+            TextView balanceTxt = findViewById(R.id.balanceText);
+            balanceTxt.setText("$"+totalBalance);
+        });
     }
 
     @Override
---
+++ app/src/main/java/sg/edu/np/mad/pennywise/dashboardViewHolder.java
@@ -0,0 +1,21 @@
+package sg.edu.np.mad.pennywise;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.w3c.dom.Text;
+
+public class dashboardViewHolder extends RecyclerView.ViewHolder{
+    TextView txt1;
+    TextView txt2;
+    TextView txt3;
+
+    public dashboardViewHolder(View itemView){
+        super(itemView);
+        txt1 = itemView.findViewById(R.id.dashTitle);
+        txt2 = itemView.findViewById(R.id.dashDate);
+        txt3 = itemView.findViewById(R.id.dashAmt);
+    }
+}
