97fef6ff77c07efdb72cad9728cc07fb3d9e1995
Koay Yeow Teik <115933415+KoayYeowTeik@users.noreply.github.com>
2023-07-20 23:34:50 +0800

    Confetti Animation

--- app/src/main/java/sg/edu/np/mad/pennywise/Goal_Progress.java
+++ app/src/main/java/sg/edu/np/mad/pennywise/Goal_Progress.java
@@ -1,18 +1,29 @@
 package sg.edu.np.mad.pennywise;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import java.util.concurrent.TimeUnit;
+
+import nl.dionsegijn.konfetti.core.PartyFactory;
+import nl.dionsegijn.konfetti.core.emitter.Emitter;
+import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
+import nl.dionsegijn.konfetti.core.models.Shape;
+import nl.dionsegijn.konfetti.core.models.Size;
+import nl.dionsegijn.konfetti.xml.KonfettiView;
+
 
 public class Goal_Progress extends AppCompatActivity {
-
     ProgressBar progressBar;
     ViewPager2 viewPager;
     LinearLayout pagerDots;
@@ -24,7 +35,6 @@ public class Goal_Progress extends AppCompatActivity {
 
         viewPager = findViewById(R.id.view_pager);
         pagerDots = findViewById(R.id.pager_dots);
-
         viewPager.setAdapter(new ProgressPagerAdapter(this));
 
         createCarouselButtons();
@@ -77,8 +87,33 @@ public class Goal_Progress extends AppCompatActivity {
     @Override
     public void onResume(){
         super.onResume();
+        KonfettiView konfettiView = findViewById(R.id.konfettiView);
+        Shape.DrawableShape drawableShape = new Shape.DrawableShape(AppCompatResources.getDrawable(this,R.drawable.ic_android_black_24dp),true);
+        EmitterConfig emitterConfig = new Emitter(300, TimeUnit.MILLISECONDS).max(300);
+        konfettiView.start(
+            new PartyFactory(emitterConfig)
+                    .shapes(Shape.Circle.INSTANCE,Shape.Square.INSTANCE, drawableShape)
+                    .spread(135)
+                    .position(0f,0f,1f,1f)
+                    .sizes(new Size(8,50,10))
+                    .timeToLive(15000)
+                    .fadeOutEnabled(true)
+                    .build()
 
+        );
 
+    }
+    private int getScreenHeight() {
+        WindowManager windowManager = getWindowManager();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.heightPixels;
+    }
+    private int getScreenWidth() {
+        WindowManager windowManager = getWindowManager();
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
     }
 
 
