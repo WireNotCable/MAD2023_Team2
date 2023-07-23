ecebbd90f8206b7795a6d8e9abaaf2fe7124ce81
Xin Yin <xinyin322@gmail.com>
2023-07-24 00:12:42 +0800

    UI and bug fixes, remove unused files

--- app/src/main/java/sg/edu/np/mad/pennywise/Goal_Progress.java
+++ app/src/main/java/sg/edu/np/mad/pennywise/Goal_Progress.java
@@ -1,122 +1,14 @@
 package sg.edu.np.mad.pennywise;
 
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.core.content.ContextCompat;
-import androidx.viewpager2.widget.ViewPager2;
-
-import java.util.concurrent.TimeUnit;
-
-import nl.dionsegijn.konfetti.core.PartyFactory;
-import nl.dionsegijn.konfetti.core.emitter.Emitter;
-import nl.dionsegijn.konfetti.core.emitter.EmitterConfig;
-import nl.dionsegijn.konfetti.core.models.Shape;
-import nl.dionsegijn.konfetti.core.models.Size;
-import nl.dionsegijn.konfetti.xml.KonfettiView;
-
 
 public class Goal_Progress extends AppCompatActivity {
-    ProgressBar progressBar;
-    ViewPager2 viewPager;
-    LinearLayout pagerDots;
-    int NUM_PAGES = 3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goal_progress);
-
-        viewPager = findViewById(R.id.view_pager);
-        pagerDots = findViewById(R.id.pager_dots);
-        viewPager.setAdapter(new ProgressPagerAdapter(this));
-
-        createCarouselButtons();
-
-        // Set a listener to handle page changes and update carousel buttons accordingly
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                updateCarouselButtons(position);
-            }
-        });
     }
-
-    private void createCarouselButtons() {
-        for (int i = 0; i < NUM_PAGES; i++) {
-            ImageView dot = new ImageView(this);
-            int size = getResources().getDimensionPixelSize(R.dimen.carousel_button_size);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(size, size);
-            int margin = getResources().getDimensionPixelSize(R.dimen.carousel_button_margin);
-            params.setMargins(margin, 0, margin, 0);
-            dot.setLayoutParams(params);
-            dot.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.page_indicator_unselected));
-            dot.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int position = pagerDots.indexOfChild(v);
-                    if (position >= 0) {
-                        viewPager.setCurrentItem(position, true);
-                    }
-                }
-            });
-            pagerDots.addView(dot);
-
-
-
-        }
-
-
-        updateCarouselButtons(0);
-    }
-
-    private void updateCarouselButtons(int selectedPosition) {
-        for (int i = 0; i < pagerDots.getChildCount(); i++) {
-            ImageView dot = (ImageView) pagerDots.getChildAt(i);
-            dot.setImageDrawable(ContextCompat.getDrawable(this,
-                    i == selectedPosition ? R.drawable.page_indicator_selected : R.drawable.page_indicator_unselected));
-        }
-    }
-
-    @Override
-    public void onResume(){
-        super.onResume();
-        KonfettiView konfettiView = findViewById(R.id.konfettiView);
-        Shape.DrawableShape drawableShape = new Shape.DrawableShape(AppCompatResources.getDrawable(this,R.drawable.ic_android_black_24dp),true);
-        EmitterConfig emitterConfig = new Emitter(300, TimeUnit.MILLISECONDS).max(300);
-        konfettiView.start(
-            new PartyFactory(emitterConfig)
-                    .shapes(Shape.Circle.INSTANCE,Shape.Square.INSTANCE, drawableShape)
-                    .spread(135)
-                    .position(0f,0f,1f,1f)
-                    .sizes(new Size(8,50,10))
-                    .timeToLive(15000)
-                    .fadeOutEnabled(true)
-                    .build()
-
-        );
-
-    }
-    private int getScreenHeight() {
-        WindowManager windowManager = getWindowManager();
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
-        return displayMetrics.heightPixels;
-    }
-    private int getScreenWidth() {
-        WindowManager windowManager = getWindowManager();
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
-        return displayMetrics.widthPixels;
-    }
-
-
-
-
 }
